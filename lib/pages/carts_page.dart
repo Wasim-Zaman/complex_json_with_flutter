e282575f7d73c2c894b742569ef3cb55393e0978
Wasim Zaman <wasimxaman13@gmail.com>
2022-12-01 12:41:24 +0500

    cart page added

---
+++ lib/pages/carts_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CartsPage extends StatelessWidget {
+  const CartsPage({super.key});
+
+  static const pageName = '/cart-page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
